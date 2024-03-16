46ab4025d8f826800a2f999118dbc35903e8c3c2
Benjamin And <s195828@student.dtu.dk>
2024-03-16 18:35:40 +0100

    Assembly info

--- obj/Debug/net8.0/Dtu-Sport_IDS.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dtu-Sport_IDS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dtu-Sport_IDS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a43b4038a5471633844e430542aa07ea4e7c7636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a065c9dbdf11b7280bbb6e2045b36f7ba8bbe56a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dtu-Sport_IDS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dtu-Sport_IDS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
